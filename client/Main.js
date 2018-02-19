8bd0540df316fdd2019a2af68eb2567812d80b63
Thomas Kelly <tmkelly28@gmail.com>
2018-02-19 13:04:23 -0500

    update starting point

--- client/Main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Main = () => (
-  <div>Your code here</div>
-)
-
-export default Main
--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Main from './Main'
+import Questions from './Questions'
 
 ReactDOM.render(
-  <Main />,
+  <Questions />,
   document.getElementById('app')
 )
