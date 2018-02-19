04409fed7175409425538addece26726856ba165
Thomas Kelly <tmkelly28@gmail.com>
2018-02-19 13:53:08 -0500

    add dummy sort house to starting point

---
+++ client/sortHouse.js
@@ -0,0 +1,7 @@
+const sortHouse = (answers) => {
+  // For now, we're all Gryffindors.
+  // Replace this with your own logic to really sort things out!
+  return 'Gryffindor'
+}
+
+export default sortHouse
